320c26df27de895b99e6a882a50124f1ddbd32be
LuisAnghel <luisanghelc23@gmail.com>
2024-05-30 19:50:14 -0500

    prueba de la api

--- screens/MapScreen/index.js
+++ screens/MapScreen/index.js
@@ -4,55 +4,29 @@ import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
 import { UserLocationContext } from "../../contextos/UserLocationContext";
 import PlaceMarker from "./PlaceMarker";
 
-const MapScreen = ({placeList}) => {
-    
-    const [mapRegion, setmapRegion] = useState([]);
+export default function MapScreen(){
 
-    const { location, setLocation } = useContext(UserLocationContext);
-  
-    useEffect(()=>{
-      if(location)
-      {
-          setmapRegion({
-              latitude: location.coords.latitude,
-              longitude: location.coords.longitude,
-              latitudeDelta: 0.0422,
-              longitudeDelta: 0.0421,
-          })
-      }
-    },[location])
-   
-  
+    const [mapRegion, setmapRegion] = useState({
+        latitude: -15.8327867,
+        longitude: -70.0320074,
+        latitudeDelta: 0.0422,
+        longitudeDelta: 0.0421, 
+    });
+
     return (
-      <View style={{ marginTop: 20 }}>
-        <Text style={{ fontSize: 20, 
-          marginBottom: 10, fontWeight: "600",fontFamily:'raleway-bold' }}>
-          Top Near By Places
-        </Text>
-        <View style={{ borderRadius: 20, overflow: "hidden" }}>
-      {location?    <MapView
+        <View style={{ marginTop: 30 }}>
+           <MapView
             style={{
               width: Dimensions.get("screen").width * 0.89,
               height: Dimensions.get("screen").height * 0.23,
+              borderRadius: 20
             }}
             provider={PROVIDER_GOOGLE}
             showsUserLocation={true}
             region={mapRegion}
-          >
-              <Marker 
-              title="You" 
-              coordinate={mapRegion}
-               />
-              {placeList.map((item,index)=>index<=4&&(
-                  <PlaceMarker item={item} key={index} />
-              ))}
-             
-          </MapView>:null} 
+            >  
+            </MapView>
           
         </View>
-       
-      </View>
     );
 }
-
-export default MapScreen;
